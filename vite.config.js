2ed0ce3a6ebe193d85f1b0b4aadfc6f567733e24
huyndse161632 <huyndse161632@fpt.edu.vn>
2025-05-29 18:55:20 +0700

    uu

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio/",
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variables.scss";`,
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
+  base: "/",
 });
